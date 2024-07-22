b1db06a81f45fbf2cfadef8ab8809246d28deabd
SergioEric <sericmorales@gmail.com>
2024-07-22 17:12:43 -0500

    feat: cv ES pdf

---
+++ pages/cv-en/index.js
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+
+export default function CV() {
+  return (
+    <div className="container">
+      <Head>
+        <title>CV-EN Devef</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="google-site-verification"
+          content="ODGjRxoZ71Gf0hj-0QnW0_gMSiqD_JR8tjPsT37GIWk"
+        />
+      </Head>
+
+      <main className="flex">
+        TODO
+        {/* <div
+          style={{
+            width: '100%',
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <iframe
+            src={`CV-ES.pdf`}
+            title="Documento PDF"
+            style={{ width: '100%', height: '100%' }}
+            frameBorder="0"
+          />
+        </div> */}
+      </main>
+    </div>
+  );
+}
--- pages/cv/index.js
+++ pages/cv/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head';
 // import { useState } from 'react';
 
 export default function CV() {
-
   // const [viewControls, setState] = useState(false);
 
   // const textViewControls = viewControls ? "Hide" : "Show";
@@ -12,55 +11,37 @@ export default function CV() {
   return (
     <div className="container">
       <Head>
-        <title>CV-Devef</title>
+        <title>CV-ES Devef</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="google-site-verification" content="ODGjRxoZ71Gf0hj-0QnW0_gMSiqD_JR8tjPsT37GIWk" />
+        <meta
+          name="google-site-verification"
+          content="ODGjRxoZ71Gf0hj-0QnW0_gMSiqD_JR8tjPsT37GIWk"
+        />
       </Head>
 
-      <main className="flex">        
-        {/* <button onClick={()=>setState(!viewControls)}>{textViewControls} Controls</button>
-        <h1>{viewControls.toString()}</h1> */}
-        <section className="flex">
-          <embed src={`cv_@sergioemr.pdf`} 
-            type="application/pdf" height="2000px" width="100%" />
-        </section>
-
-
+      <main className="flex">
+        <div
+          style={{
+            width: '100%',
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <iframe
+            src={`CV-ES.pdf`}
+            title="Documento PDF"
+            style={{ width: '100%', height: '100%' }}
+            frameBorder="0"
+          />
+        </div>
+        {/* <section className="flex">
+          <embed src={`cv_@sergioemr.pdf`} type="application/pdf" height="2000px" width="100%" />
+        </section> */}
       </main>
 
-      <style jsx>{`
-      .title{
-        text-align: center;
-      }
-
-      .flex{
-        // display: flex;
-        // justify-content: center;
-        // flex-direction: column;
-        // align-items:center;
-        // height: 100vh;
-      }
-
-      main{
-        // background-color:#131313;
-      }
-
-      `}</style>
-
-      <style jsx>{`
-        // html,
-        // body {
-        //   padding: 0;
-        //   margin: 0;
-        //   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-        //     Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-        //     sans-serif;
-        //   background-color:#131313;
-        // }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      <style jsx>{``}</style>
     </div>
-  )
+  );
 }
